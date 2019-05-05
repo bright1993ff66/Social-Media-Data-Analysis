4fa6c3174561733a3076bb3b901dc7e7b9c2dd98
Bright Chang <75425626@qq.com>
2019-05-05 16:20:10 +0800

    Cope with the userwarning issue

--- Visualization/WordCloud.py
+++ Visualization/WordCloud.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import os, re
 import read_data
+import string
 
 import gensim
 from gensim import corpora, models
@@ -28,6 +29,18 @@ unuseful_terms_set = set(unuseful_terms)
 
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
+# the regex used to detect words is a combination of normal words, ascii art, and emojis
+# 2+ consecutive letters (also include apostrophes), e.x It's
+normal_word = r"(?:\w[\w']+)"
+# 2+ consecutive punctuations, e.x. :)
+ascii_art = r"(?:[{punctuation}][{punctuation}]+)".format(punctuation=string.punctuation)
+# a single character that is not alpha_numeric or other ascii printable
+emoji = r"(?:[^\s])(?<![\w{ascii_printable}])".format(ascii_printable=string.printable)
+regexp = r"{normal_word}|{ascii_art}|{emoji}".format(normal_word=normal_word, ascii_art=ascii_art,
+                                                     emoji=emoji)
+symbola_font_path = os.path.join(read_data.plot_path, 'symbola.ttf')
+
+circle_mask = np.array(Image.open(r"F:\CityU\Datasets\Hong Kong Tweets 2017\circle.png"))
 
 # Change the color of the wordcloud
 def green_func(word, font_size, position, orientation, random_state=None,
@@ -74,18 +87,23 @@ def process_words(texts, stop_words, bigram_mod, trigram_mod, allowed_postags=['
 
 
 def create_text_for_wordcloud(df):
+    """
+    :param df: the pandas dataframe which contains the text of tweets
+    :return: the process text for word cloud generation
+    """
     tweet_text = list(df['cleaned_text'])
     tokenized_text_list = [word_tokenize(text) for text in tweet_text]
-    bigram = models.Phrases(tokenized_text_list, min_count=5,
+    bigram = models.phrases.Phrases(tokenized_text_list, min_count=5,
                                    threshold=100)  # higher threshold fewer phrases.
-    trigram = models.Phrases(bigram[tokenized_text_list])
     bigram_mod = models.phrases.Phraser(bigram)
+    trigram = models.phrases.Phrases(bigram_mod[tokenized_text_list])
     trigram_mod = models.phrases.Phraser(trigram)
     processed_text = process_words(tokenized_text_list, bigram_mod=bigram_mod, trigram_mod=trigram_mod,
                                stop_words=unuseful_terms_set)
     text_in_list = [' '.join(text) for text in processed_text]
     text_ready = ' '.join(text_in_list)
     return text_ready
+
 
 # Delete users which have same geoinformation and the total number of posted tweets is bigger than 10
 def delete_bots_have_same_geoinformation(df):
@@ -103,10 +121,8 @@ def delete_bots_have_same_geoinformation(df):
     return cleaned_df
 
 
-
 if __name__ == '__main__':
     # Load a mask
-    circle_mask = np.array(Image.open(r"F:\CityU\Datasets\Hong Kong Tweets 2017\circle.png"))
     whole_tweets = pd.read_pickle(os.path.join(read_data.tweet_2017, 'final_2017_with_sentiment_smote.pkl'))
     # Just in case: filter out the users whose tweets have exactly the same geoinformation
     whole_tweets_filterd = delete_bots_have_same_geoinformation(whole_tweets)

14b7ccb88399e00cf0ee8134168866ee5bf603ce
Bright Chang <75425626@qq.com>
2019-05-21 00:35:25 +0800

    update the codes for cross sectional study

--- transit_non_transit_comparision/cross_sectional_study.py
+++ transit_non_transit_comparision/cross_sectional_study.py
@@ -18,13 +18,21 @@ cross_sectional_study_station_names = ['Admiralty', 'Airport', 'Austin', 'Causew
                                        'Mong Kok East', 'Prince Edward', 'Sai Ying Pun', 'Sheung Wan',
                                        'Tai Koo', 'Tin Hau', 'Tsim Sha Tsui', 'Wan Chai', 'Whampoa',
                                        'Yau Ma Tei']
+region_one = ['Airport', 'Disneyland']
+region_two = ['Kennedy Town', 'HKU',  'Sai Ying Pun', 'Sheung Wan', 'Hong Kong', 'Central', 'Admiralty',
+              'Wan Chai', 'Causeway Bay', 'Tin Hau', 'Tai Koo']
+region_three = ['Austin', 'East Tsim Sha Tsui', 'Hung Hom', 'Jordan', 'Kowloon', 'Mong Kok',
+                'Mong Kok East', 'Prince Edward', 'Tsim Sha Tsui',  'Whampoa', 'Yau Ma Tei']
+region_four = ['Kwun Tong']
+
+
 # Hong Kong and Shanghai share the same time zone.
 # Hence, we transform the utc time in our dataset into Shanghai time
 time_zone_hk = pytz.timezone('Asia/Shanghai')
 tns_path = os.path.join(read_data.transit_non_transit_comparison_cross_sectional, 'tns_dataframe')
 
 
-class TransitNeighborhood_before_after(object):
+class TransitNeighborhood_cross_sectional(object):
 
     def __init__(self, tn_dataframe, non_tn_dataframe, oct_open, before_and_after, compute_positive,
                  compute_negative):
@@ -105,8 +113,29 @@ class TransitNeighborhood_before_after(object):
         ax.set_xticks(x)
         ax.set_xticklabels(time_list, rotation='vertical')
         plt.title(plot_title_name)
-        plt.savefig(os.path.join(read_data.transit_non_transit_comparison_before_after, saving_file_name))
+        plt.savefig(os.path.join(read_data.transit_non_transit_comparison_cross_sectional, saving_file_name))
         plt.show()
+
+
+def create_dataframe_dicts_for_four_regions(region1_name_list, region2_name_list, region3_name_list, region4_name_list):
+    region1_dict = {}
+    region2_dict = {}
+    region3_dict = {}
+    region4_dict = {}
+    regions_name_list = [region1_name_list, region2_name_list, region3_name_list, region4_name_list]
+    for index, region in enumerate(regions_name_list):
+        for station_name in region:
+            dataframe =  before_and_after.get_tweets_based_on_date(station_name, start_date, end_date)
+            dataframe.to_pickle(os.path.join(tns_path, station_name+'.pkl'))
+            if index+1 == 1:
+                region1_dict[station_name] = dataframe
+            elif index+1 == 2:
+                region2_dict[station_name] = dataframe
+            elif index+1 == 3:
+                region3_dict[station_name] = dataframe
+            else:
+                region4_dict[station_name] = dataframe
+    return region1_dict, region2_dict, region3_dict, region4_dict
 
 
 if __name__ == '__main__':
@@ -117,8 +146,72 @@ if __name__ == '__main__':
     start_date = datetime(2016, 5, 7, tzinfo=time_zone_hk)
     end_date = datetime(2017, 12, 31, tzinfo=time_zone_hk)
 
-    dataframes_list = []
-    for index, station_name in enumerate(cross_sectional_study_station_names):
-        dataframe_for_one_station = before_and_after.get_tweets_based_on_date(station_name, start_date, end_date)
-        dataframe_for_one_station.to_pickle(os.path.join(tns_path, cross_sectional_study_station_names[index]+'.pkl'))
-        dataframes_list.append(dataframe_for_one_station)
+    region1_dataframe_dict, region2_dataframe_dict, region3_dataframe_dict, region4_dataframe_dict = \
+        create_dataframe_dicts_for_four_regions(region_one, region_two, region_three, region_four)
+
+    non_tn_path = os.path.join(read_data.transit_non_transit_comparison_cross_sectional, 'non_tns_dataframe')
+    region1_nontn_dataframe = pd.read_csv(os.path.join(non_tn_path, 'region_one_tpu_nontn.csv'),
+                                          encoding='latin-1')
+    region2_nontn_dataframe = pd.read_csv(os.path.join(non_tn_path, 'region_two_tpu_nontn.csv'),
+                                          encoding='latin-1')
+    region3_nontn_dataframe = pd.read_csv(os.path.join(non_tn_path, 'region_three_tpu_nontn.csv'),
+                                          encoding='latin-1')
+    region4_nontn_dataframe = pd.read_csv(os.path.join(non_tn_path, 'region_four_tpu_nontn.csv'),
+                                          encoding='latin-1')
+
+    for name in list(region1_dataframe_dict.keys()):
+        tn_object = TransitNeighborhood_cross_sectional(tn_dataframe=region1_dataframe_dict[name],
+                                                        non_tn_dataframe=region1_nontn_dataframe,
+                                                     before_and_after=False, oct_open=False, compute_positive=False,
+                                                     compute_negative=False)
+        tn_object.line_map_comparison(line_labels=('Sentiment Level of '+name+' TN', 'Sentiment Level of Non-TN'),
+                                   ylabel='Percentage of Positive Tweets Minus Percentage of Negative Tweets',
+                                   plot_title_name='Sentiment Cross Sectional Study: '+name,
+                                   saving_file_name=name+'_sent_compare.png', draw_sentiment=True)
+        tn_object.line_map_comparison(line_labels=('Activity Level of ' + name + ' TN', 'Activity Level of Non-TN'),
+                                      ylabel='Number of Posted Tweets',
+                                      plot_title_name='Activity Cross Sectional Study: ' + name,
+                                      saving_file_name=name + '_act_compare.png', draw_sentiment=False)
+
+    for name in list(region2_dataframe_dict.keys()):
+        tn_object = TransitNeighborhood_cross_sectional(tn_dataframe=region2_dataframe_dict[name],
+                                                        non_tn_dataframe=region2_nontn_dataframe,
+                                                     before_and_after=False, oct_open=False, compute_positive=False,
+                                                     compute_negative=False)
+        tn_object.line_map_comparison(line_labels=('Sentiment Level of '+name+' TN', 'Sentiment Level of Non-TN'),
+                                   ylabel='Percentage of Positive Tweets Minus Percentage of Negative Tweets',
+                                   plot_title_name='Sentiment Cross Sectional Study: '+name,
+                                   saving_file_name=name+'_sent_compare.png', draw_sentiment=True)
+        tn_object.line_map_comparison(line_labels=('Activity Level of ' + name + ' TN', 'Activity Level of Non-TN'),
+                                      ylabel='Number of Posted Tweets',
+                                      plot_title_name='Activity Cross Sectional Study: ' + name,
+                                      saving_file_name=name + '_act_compare.png', draw_sentiment=False)
+
+    for name in list(region3_dataframe_dict.keys()):
+        tn_object = TransitNeighborhood_cross_sectional(tn_dataframe=region3_dataframe_dict[name],
+                                                        non_tn_dataframe=region3_nontn_dataframe,
+                                                     before_and_after=False, oct_open=False, compute_positive=False,
+                                                     compute_negative=False)
+        tn_object.line_map_comparison(line_labels=('Sentiment Level of '+name+' TN', 'Sentiment Level of Non-TN'),
+                                   ylabel='Percentage of Positive Tweets Minus Percentage of Negative Tweets',
+                                   plot_title_name='Sentiment Cross Sectional Study: '+name,
+                                   saving_file_name=name+'_sent_compare.png', draw_sentiment=True)
+        tn_object.line_map_comparison(line_labels=('Activity Level of ' + name + ' TN', 'Activity Level of Non-TN'),
+                                      ylabel='Number of Posted Tweets',
+                                      plot_title_name='Activity Cross Sectional Study: ' + name,
+                                      saving_file_name=name + '_act_compare.png', draw_sentiment=False)
+
+    for name in list(region4_dataframe_dict.keys()):
+        tn_object = TransitNeighborhood_cross_sectional(tn_dataframe=region4_dataframe_dict[name],
+                                                        non_tn_dataframe=region4_nontn_dataframe,
+                                                     before_and_after=False, oct_open=False, compute_positive=False,
+                                                     compute_negative=False)
+        tn_object.line_map_comparison(line_labels=('Sentiment Level of '+name+' TN', 'Sentiment Level of Non-TN'),
+                                   ylabel='Percentage of Positive Tweets Minus Percentage of Negative Tweets',
+                                   plot_title_name='Sentiment Cross Sectional Study: '+name,
+                                   saving_file_name=name+'_sent_compare.png', draw_sentiment=True)
+        tn_object.line_map_comparison(line_labels=('Activity Level of ' + name + ' TN', 'Activity Level of Non-TN'),
+                                      ylabel='Number of Posted Tweets',
+                                      plot_title_name='Activity Cross Sectional Study: ' + name,
+                                      saving_file_name=name + '_act_compare.png', draw_sentiment=False)
+
